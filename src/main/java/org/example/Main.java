8cc661e62cabd8280e1933bf3bd4bc36e17f5ecc
Anas Ahmad <ahmedanas8032@gmail.com>
2024-04-29 00:16:59 +0530

    Updated dashboard line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My Calculator");
+        System.out.println("My Calculator Application");
         Calculator calculator = new Calculator();
         System.out.println("Addition: " + calculator.add(5, 3));
         System.out.println("Subtraction: " + calculator.subtract(5, 3));
